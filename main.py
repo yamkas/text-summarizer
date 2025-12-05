83cf5bdfa53631aec32c6fc3c56bc5126b1093d0
yamkas <yamkasudirman@gmail.com>
2025-12-05 19:25:06 +0700

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_1_data_ingestion import DataIngestionPipeline
 
-logger.info("Starting the text summarization process.")
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion_pipeline=DataIngestionPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+PARAMS_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH= Path("config/config.yaml")
