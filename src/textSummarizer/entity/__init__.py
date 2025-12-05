46e5acf4ba3e8ec126fa612bdccf7af6f60e4110
yamkas <yamkasudirman@gmail.com>
2025-12-05 19:25:06 +0700

    data ingestion

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: Path
+    local_data_file: Path
+    unzip_dir: Path
+
---
+++ src/textSummarizer/pipeline/stage_1_data_ingestion.py
@@ -0,0 +1,14 @@
+from src.textSummarizer.components.data_ingestion import DataIngestion
+from src.textSummarizer.config.configuration import ConfigurationManager
+
+
+class DataIngestionPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_ingestion(self):
+        config=ConfigurationManager()
+        data_ingestion_config=config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.downlaod_file()
+        data_ingestion.extract_zip_file()
